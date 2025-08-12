2393e6da2a6f713887b411b5a8b4e0e5e1173f96
RiteshRautela <rautelaritesh2gmail.com>
2025-08-12 19:47:50 +0530

    tailwindcss + daisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./component/Navbar"
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-   <h1>hellow world</h1>
+    <Navbar/>
+   <h1 class="text-3xl text-center" >hellow world</h1>
     </>
   )
 }
+
 
 export default App
